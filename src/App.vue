a2e971decd1f7ef154231809de93649117423db4
JonatanCuri07 <u201912404@upc.edu.pe>
2026-07-28 11:10:23 -0500

    chore: configuracion del proyecto

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex min-h-screen items-center justify-center bg-violet-700">
+    <h1 class="text-4xl font-bold text-white">Conversor de moneda</h1>
+  </main>
+</template>
